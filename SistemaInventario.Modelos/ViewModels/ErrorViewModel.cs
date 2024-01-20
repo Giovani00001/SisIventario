78a591e9999631b9333169312a3d08ff4db8cae6
Giovani <Giip00001@gmail.com>
2024-01-19 23:54:00 -0600

    Configuracion del proyecto

---
+++ SistemaInventario.Modelos/ViewModels/ErrorViewModel.cs
@@ -0,0 +1,10 @@
+namespace SistemaInventario.Modelos.ViewModels
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
