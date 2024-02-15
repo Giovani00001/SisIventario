5d04cd7f84e665b03b45e1e2bda0891918497378
Giovani <Giip00001@gmail.com>
2024-02-14 18:42:56 -0600

    Administracion de Usuario

---
+++ SistemaInventario/Areas/Admin/Controllers/UsuarioController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.DotNet.Scaffolding.Shared.Messaging;
+using Microsoft.EntityFrameworkCore;
+using SistemaInventario.AccesoDatos.Data;
+using SistemaInventario.AccesoDatos.Repositorio.IRepositorio;
+
+namespace SistemaInventario.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UsuarioController : Controller
+    {
+        private readonly IUnidadTrabajo _unidadTrabajo;
+        private readonly ApplicationDbContext _db;
+        public UsuarioController(IUnidadTrabajo unidadTrabajo, ApplicationDbContext applicationDbContext)
+        {
+            _unidadTrabajo = unidadTrabajo;
+            _db = applicationDbContext;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API
+        [HttpGet]
+        public async Task<IActionResult> ObtenerTodos()
+        {
+            var usuarioLista = await _unidadTrabajo.UsuarioAplicacion.ObtenerTodos();
+            var userRole = await _db.UserRoles.ToListAsync();
+            var roles = await _db.Roles.ToListAsync();
+
+            foreach (var usr in usuarioLista)
+            {
+                var roleId = userRole.FirstOrDefault(u => u.UserId == usr.Id).RoleId;
+                usr.Role = roles.FirstOrDefault(r => r.Id == roleId).Name;
+            }
+            return Json(new { data = usuarioLista });
+
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> BloquearDesbloquear([FromBody] string id)
+        {
+            var usuario = await _unidadTrabajo.UsuarioAplicacion.ObtenerPrimero(p => p.Id == id);
+            if (usuario == null)
+            {
+                return Json(new { success = false, message = "Erorr en el usuario" });
+            }
+            if (usuario.LockoutEnd != null && usuario.LockoutEnd > DateTime.Now)
+            {
+                usuario.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                usuario.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+            await _unidadTrabajo.Guardar();
+
+            return Json(new { success = true, message = "Ooperacion exitosa" });
+        }
+
+        #endregion
+
+
+
+
+    }
+}
--- SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
+++ SistemaInventario/obj/Debug/net7.0/SistemaInventario.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16743f1b903ac23649e2842bb2b56b331fae7d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3e5e7e897f7861f3f76543e365cee6a70814415")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaInventario")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
